ac3a677da67e2ed784156ac49a04e83032c8f5ed
hoangtran <tranbaohoang70@gmail.com>
2023-11-17 14:01:53 +0700

    xóa rác

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const authController = require("../controller/authController");
-const router = express.Router();
-
-router.get("/", authController.getAllUsers);
-router.post("/register",authController.registerUser);
-module.exports=router;
